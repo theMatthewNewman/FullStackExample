3eea1e1cc0b3e9363237e15d965c3fd74736b92e
Matthew Newman <theMatthewNewman@gmail.com>
2022-10-26 14:07:31 -0400

    mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -10,4 +15,8 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 app.use('/', indexRouter)
 
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open',() => console.log("Connected to mongoose"))
 app.listen(process.env.PORT || 3000)
